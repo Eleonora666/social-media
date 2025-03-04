6684205c82c2c590508751a77edf4a97895016c8
Eleonora Jevdokimova <elja2005@icloud.com>
2025-03-04 18:07:12 +0100

    message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-
 import HackerAuth from "./Auth/Auth"; 
 import Profile from "./profile/Profile";
 
 function App() {
   return (
-    <Router> {/* Use BrowserRouter as Router */}
+    <Router> 
       <Routes>
         <Route path="/" element={<HackerAuth />} /> 
         <Route path="/profile" element={<Profile />} /> 
-        {/* Add other routes here */}
       </Routes>
     </Router>
   );
